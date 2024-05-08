5d5d24d0453945f0fbe5c47464b534584b9ec7b0
SeanOnamade <100744222+SeanOnamade@users.noreply.github.com>
2024-05-08 11:20:58 -0600

    remove quotes lol

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    apiKey: 'process.env.API_KEY',
-    mapApi: 'process.env.MAP_API_KEY'
+    apiKey: process.env.API_KEY,
+    mapApi: process.env.MAP_API_KEY
 };
 
 export default config;

80fef0510f5f5697641acf1c3537198415b5435d
SeanOnamade <100744222+SeanOnamade@users.noreply.github.com>
2024-05-08 11:57:07 -0600

    specify paths

--- replace-env.js
+++ replace-env.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 // Read JavaScript file
-let content = fs.readFileSync('path/to/your/javascript/file.js', 'utf8');
+let content = fs.readFileSync('weather.js', 'utf8');
 
 // Replace placeholders with environment variable values
 content = content.replace(/%API_KEY%/g, process.env.API_KEY);
 content = content.replace(/%MAP_API_KEY%/g, process.env.MAP_API_KEY);
 
 // Write modified content to a build directory
-fs.writeFileSync('path/to/your/build/directory/file.js', content);
+fs.writeFileSync('weather.js', content);

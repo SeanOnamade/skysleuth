abbba02964cd793b6d792d2110f6561377ebb060
SeanOnamade <100744222+SeanOnamade@users.noreply.github.com>
2024-05-08 11:53:30 -0600

    % signs

--- replace-env.js
+++ replace-env.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
 // Read JavaScript file
-const content = fs.readFileSync('path/to/your/javascript/file.js', 'utf8');
+let content = fs.readFileSync('path/to/your/javascript/file.js', 'utf8');
 
 // Replace placeholders with environment variable values
-const replacedContent = content.replace(/%API_KEY%/g, process.env.API_KEY);
+content = content.replace(/%API_KEY%/g, process.env.API_KEY);
+content = content.replace(/%MAP_API_KEY%/g, process.env.MAP_API_KEY);
 
 // Write modified content to a build directory
-fs.writeFileSync('path/to/your/build/directory/file.js', replacedContent);
+fs.writeFileSync('path/to/your/build/directory/file.js', content);
